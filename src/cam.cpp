50b4fd6275bdc00dc55b92ccb1d128bee8636536
Martijn Veldpaus <martijnv@phact.nl>
2017-05-11 11:21:01 +0200

    linker

--- src/cam.cpp
+++ src/cam.cpp
@@ -23,10 +23,12 @@ void CCam::Start()
       std::cout << "Set framerate" << std::endl;
 
       cv::namedWindow("frame");
+      std::cout << "Created window" << std::endl;
 
       cv::Mat frame;
       while(capture.read(frame))
       {
+        std::cout << "read frame" << std::endl;
         cv::imshow("frame", frame);
         cv::waitKey(1);
       }
