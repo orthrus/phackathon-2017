da5647f64eb18ed7d717faecab51787104eeef15
Martijn Veldpaus <martijnv@phact.nl>
2017-05-11 11:31:47 +0200

    linker

--- src/cam.cpp
+++ src/cam.cpp
@@ -1,6 +1,7 @@
 
 #include "cam.h"
 #include <iostream>
+#include <thread>
 
 CCam::CCam()
 {
@@ -27,14 +28,25 @@ void CCam::Start()
       cv::namedWindow("frame");
       std::cout << "Created window" << std::endl;
 
-      cv::Mat frame;
-      while(_capture.read(frame))
+      std::thread t = std::thread([=]() mutable
+    	{
+    		int i = 0;
+    		cv::Mat frame;
+    		while (capture.read(frame))
+    		{
+    			std::stringstream ss;
+    			cv::rectangle(frame, cv::Point(10, 2), cv::Point(100, 20), cv::Scalar(255, 255, 255), -1);
+    			ss << capture.get(CV_CAP_PROP_POS_FRAMES);
+    			std::string frameNumberString = ss.str();
+    			cv::putText(frame, frameNumberString.c_str(), cv::Point(15, 15), CV_FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar(0, 0, 0));
+    			cv::imshow("frame", frame);
+    		}
+    	});
+
+      while(true)
       {
-        cv::imshow("frame", frame);
         cv::waitKey(1);
       }
-
-
     }
 
     _capture.release();
