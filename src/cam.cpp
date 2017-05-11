82f72f2896256eff430b6548824a452cea28ce85
Martijn Veldpaus <martijnv@phact.nl>
2017-05-11 11:57:22 +0200

    linker

--- src/cam.cpp
+++ src/cam.cpp
@@ -31,27 +31,28 @@ void CCam::Start()
       _running = true;
       std::thread t = std::thread([=]() mutable
     	{
-    		int i = 0;
-    		cv::Mat frame;
-    		while (_capture.read(frame))
-    		{
-    			//std::stringstream ss;
-    			//cv::rectangle(frame, cv::Point(10, 2), cv::Point(100, 20), cv::Scalar(255, 255, 255), -1);
-    			//ss << _capture.get(CV_CAP_PROP_POS_FRAMES);
-          //ss << i++;
-    			//std::string frameNumberString = ss.str();
-    			//cv::putText(frame, frameNumberString.c_str(), cv::Point(15, 15), CV_FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar(0, 0, 0));
-    			cv::imshow("frame", frame);
-    		}
-
-        std::cout << "Stopped reading" << std::endl;
-        _running = false;
+        while(_running)
+        {
+          cv::waitKey(1);
+        }
     	});
 
-      while(_running)
+      int i = 0;
+      cv::Mat frame;
+      while (_capture.read(frame))
       {
-        cv::waitKey(1);
+        //std::stringstream ss;
+        //cv::rectangle(frame, cv::Point(10, 2), cv::Point(100, 20), cv::Scalar(255, 255, 255), -1);
+        //ss << _capture.get(CV_CAP_PROP_POS_FRAMES);
+        //ss << i++;
+        //std::string frameNumberString = ss.str();
+        //cv::putText(frame, frameNumberString.c_str(), cv::Point(15, 15), CV_FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar(0, 0, 0));
+        cv::imshow("frame", frame);
       }
+
+      std::cout << "Stopped reading" << std::endl;
+      _running = false;
+
 
       t.join();
     }
